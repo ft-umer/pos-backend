c59a4869c1da563299f5aba5784dfd47dace115f
Syed Umer Mujahid Hassni <109479332+ft-umer@users.noreply.github.com>
2025-10-20 14:06:38 +0500

    backend

--- server.js
+++ server.js
@@ -16,18 +16,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(
-  cors({
-    origin: ["https://www.egencydigital.com", "http://localhost:5173"],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
-app.options("*", cors()); // ✅ handle preflight safely
-
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
